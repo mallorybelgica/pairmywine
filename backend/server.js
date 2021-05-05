80b7923436e14dd3fbc299e6d12a4c81a50e3541
Mallory Belgica <mallorybelgica@gmail.com>
2021-05-05 15:23:46 -0400

    added cors

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(cors({ origin: "https://pairmywine-app.netlify.app" }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(require("./routes"));
