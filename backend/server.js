c210189242780e9531fcb96b656716ff91b61dcc
Mallory Belgica <mallorybelgica@gmail.com>
2021-05-05 14:10:35 -0400

    updated port

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,17 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(require("./routes"));
+
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "Greetings from the backend!" });
+});
 
 app.listen(PORT, function () {
   console.info(`🌍 Listening on port ${PORT}`);
